73fd7359b3d3c25a20f49d548a61714f1e0957c7
David Zuluaga <davidzuluaga1991@gmail.com>
2023-09-28 23:46:23 -0500

    Changes in environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://catalog-product-back-k0toqoap6-davidzuluaga1991.vercel.app/',
+  uri: 'https://catalog-product-back-k0toqoap6-davidzuluaga1991.vercel.app/',
 };
